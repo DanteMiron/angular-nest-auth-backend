4c06ffdb718444816b76270ddf64e1bbee196ff3
DanteMiron <dantemiron@hotmail.com>
2024-04-01 12:09:06 -0300

    finish backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot(),
+      MongooseModule.forRoot(process.env.MONGO_URI, {
+        dbName: process.env.MONGO_DB_NAME
+      }),
+      AuthModule
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+  constructor(){
+    console.log(process.env)
+  }
+}
